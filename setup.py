3c6e681599e51cd60f8bc7d3d63f02e5480ce8f2
hrshitagowda <78568667+hrshitagowda@users.noreply.github.com>
2024-10-16 19:56:26 -0400

    Update setup.py
    Updated the url and the package name

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ import os
 import re
 import setuptools
 
-with open('molalkit/__init__.py') as fd:
-    __version__ = re.search("__version__ = '(.*)'", fd.read()).group(1)
-
+# Hardcoding version instead of dynamically fetching from __init__.py
+__version__ = '1.0.0'  # Update the version number if needed
 
 def read(*filenames, **kwargs):
     encoding = kwargs.get('encoding', 'utf-8')
@@ -16,11 +15,11 @@ def read(*filenames, **kwargs):
             buf.append(f.read())
     return sep.join(buf)
 
-
+# Reading the long description from README.md
 long_description = read('README.md')
 
 setuptools.setup(
-    name='molalkit',
+    name='MolALKit_1_0',  # Updated package name
     version=__version__,
     python_requires='>=3.8',
     install_requires=[
@@ -38,7 +37,8 @@ setuptools.setup(
     author_email='yan.xiang@duke.edu',
     description='MolALKit: A Toolkit for Active Learning in Molecular Data.',
     long_description=long_description,
-    url='https://github.com/RekerLab/MolALKit',
+    long_description_content_type='text/markdown',  # Added for markdown support
+    url='https://github.com/hrshitagowda/MolALKit_1_0',  # Updated URL to your fork
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python',
